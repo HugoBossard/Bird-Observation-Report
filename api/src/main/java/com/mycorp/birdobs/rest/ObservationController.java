ff31d37818886625ef3b6902a5646a97a5308cf6
Hugo Bossard <hbossard01@gmail.com>
2024-12-08 18:06:01 +0100

    Ajout : route PUT et DELETE pour modifier et supprimer une observation

--- api/src/main/java/com/mycorp/birdobs/rest/ObservationController.java
+++ api/src/main/java/com/mycorp/birdobs/rest/ObservationController.java
@@ -3,9 +3,11 @@ package com.mycorp.birdobs.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +50,35 @@ public class ObservationController {
         }
 
         return new ResponseEntity<>(observationDto, HttpStatus.OK);
+    }
+
+    @PutMapping(VERIFY_ID)
+    public ResponseEntity<?> updateObservationReportById(@PathVariable Integer id, @RequestBody ObservationDto observation) {
+        if (observation.getReportID() != null) {
+            return new ResponseEntity<>("Vous ne pouvez pas modifier l'ID.", HttpStatus.BAD_REQUEST);
+        }
+
+        if (observation.getDatePub() != null) {
+            return new ResponseEntity<>("Vous ne pouvez pas modifier la date.", HttpStatus.BAD_REQUEST);
+        }
+        
+        ObservationDto observationUpdated = observationService.updateById(id, observation);
+
+        if (observationUpdated == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(observationUpdated, HttpStatus.OK);
+    }
+
+    @DeleteMapping(VERIFY_ID)
+    public ResponseEntity<Void> deleteObservationReportById(@PathVariable Integer id) {
+        boolean found = observationService.deleteById(id);
+
+        if (!found) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
